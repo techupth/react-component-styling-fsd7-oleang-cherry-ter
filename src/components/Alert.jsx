6c9313bfb41faf76989ccafd3fd8a41a56ef3014
Orayanee Janjaeng <orayaneejj@gmail.com>
2024-10-22 14:28:27 +0700

    feat: add and style buttons and alert boxes

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,44 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import errorIcon from "/src/assets/error-circle.svg";
+import warningIcon from "/src/assets/alert-triangle.svg";
+import infoIcon from "/src/assets/alert-circle.svg";
+import successIcon from "/src/assets/check-circle.svg";
+function Alert({ type }) {
+  let icon;
+  let backgroundColor;
+  if (type === "error") {
+    icon = errorIcon;
+    backgroundColor = "#F9C8C8";
+  } else if (type === "warning") {
+    icon = warningIcon;
+    backgroundColor = "#F9D9C8";
+  } else if (type === "info") {
+    icon = infoIcon;
+    backgroundColor = "#F9EBC8";
+  } else {
+    icon = successIcon;
+    backgroundColor = "#CEF7CD";
+  }
 
-function Alert() {}
+  const buttonStyles = css`
+    width: 650px;
+    height: 76px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 10px;
+    color: #444444;
+    font-weight: 700;
+    background-color: ${backgroundColor};
+  `;
+
+  return (
+    <button css={buttonStyles}>
+      <img src={icon} />
+      This is {type} alert box
+    </button>
+  );
+}
+export default Alert;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,13 @@
 // Start coding here
-
-function Button() {}
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+function Button({ type }) {
+  const buttonStyles = css`
+  width: 171.19px;
+  height: 50px;
+    color: white;
+    background-color: ${type === "primary" ? "#074EE8" : "#07A4E8"};
+  `;
+  return <button css={buttonStyles}>Button</button>;
+}
+export default Button;
