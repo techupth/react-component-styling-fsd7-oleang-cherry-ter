8be7cb91638470f54eaf043d67482e636e71fd04
OLEANG <wannachok.job@gmail.com>
2024-10-22 13:31:19 +0700

    answer-for-component-styling

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,52 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Alert() {}
+const alerts = [
+  { status: "Default", color: "black" },
+  { status: "Hover", color: "blue" },
+  { status: "Error", color: "red" },
+  { status: "Focus", color: "purple" },
+];
+
+function Alert() {
+  return (
+    <>
+      {alerts.map((item, index) => (
+        <div key={index} css={css`
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+        `}>
+          <h1>{item.status}</h1>
+          <h2>Email Address</h2>
+          <input
+            type="email"
+            placeholder="Enter email address here"
+            css={css`
+                border-color: ${item.color};
+                padding-right: 80px;
+                padding-top: 20px
+                border-radius: 20px;
+            `}
+          />
+          <h5>We'll never share your email.</h5>
+          <h2>Username</h2>
+          <input
+            type="text"
+            placeholder="Enter Username here"
+            css={css`
+                border-color: ${item.color};
+                padding-right: 80px;
+                padding-top: 20px
+                border-radius: 20px;
+            `}
+          />
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default Alert;
+
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,49 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+function Button({ type }) {
+  return (
+    <div
+      css={css`
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+      `}
+    >
+      {type === "primary" ? (
+        <button
+          css={css`
+            padding-left: 60px;
+            background-color: #074ee8;
+            font-size: 24px;
+            border-radius: 4px;
+            color: white;
+            &:hover {
+              color: black;
+            }
+          `}
+        >
+          Primary button
+        </button>
+      ) : (
+        <button
+          css={css`
+            padding-left: 30px;
+            background-color: #07a4e8;
+            font-size: 24px;
+            border-radius: 4px;
+            color: white;
+            &:hover {
+              color: black;
+            }
+          `}
+        >
+          Secondary button
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default Button;
